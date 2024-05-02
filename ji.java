986cbf436554c875ebd0a44b9418a4c17f28d778
Nandna1305 <167091623+Nandna1305@users.noreply.github.com>
2024-05-02 19:46:16 +0530

    Update ji.java

--- ji.java
+++ ji.java
@@ -1,5 +1,7 @@
 public class ji {
+    //program for left half pyramid
     public static void main (String args[]) {
+        //first loop is for printing rows,second loop is for printing empty spaces
         for (int i=0;i<=5;i++)
         {
             for (int j=5;j>=i;j--)
@@ -7,6 +9,7 @@ public class ji {
               System.out.print( " ");
             
             }
+            // third loop is for printing asterisk
             for( int k=0;k<=i;k++)
             {
                 System.out.print("*");
