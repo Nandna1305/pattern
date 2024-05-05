14f4eb097aae37e542e96d37fbc7455ef7f423bc
Nandna1305 <167091623+Nandna1305@users.noreply.github.com>
2024-05-05 07:30:30 +0530

    Add files via upload

---
+++ rose.java
@@ -0,0 +1,26 @@
+public class rose 
+{
+    public static void main (String args[]) 
+    {
+        //inner loop is for managing the no.of.columns and for creating required spaces
+        //outer loop is for rows
+        for (int i=1;i<=4;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+              System.out.print( " ");
+            
+            }
+            //this loop is for printing the asterisk
+            for(int k=1;k<=4;k++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        
+        }
+
+    }
+    
+}
+
